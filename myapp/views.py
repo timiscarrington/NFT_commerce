ce79f7281bcb3b6178a9451d502acf78b948b5ce
timiscarrington <timiscarrington@gmail.com>
2023-03-24 11:47:31 -0700

    created product cards, doing some styling, need to create the detail page next

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 def index(request):
-    return render(request, 'myapp/index.html')
+    products = Product.objects.all()
+    return render(request, 'myapp/index.html', {'products':products})
